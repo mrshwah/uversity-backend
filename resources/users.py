c99f6fdd57e42106b8a38fa421745b28ba2a85be
Nick Chouard <nick.chouard@gmail.com>
2019-01-26 09:01:29 -0600

    Added decorator to check user permission

--- resources/users.py
+++ resources/users.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required
+from utils.decorators import check_jwt_identity
 from services.users import update_user, delete_user, get_user
 
 put_parser = reqparse.RequestParser()
@@ -18,6 +19,7 @@ class User(Resource):
         return user
 
     @jwt_required
+    @check_jwt_identity
     def put(self, user_id):
         args = put_parser.parse_args()
         try:
@@ -28,6 +30,7 @@ class User(Resource):
         return {'user': updated_user}
 
     @jwt_required
+    @check_jwt_identity
     def delete(self, user_id):
         try:
             resp = delete_user(user_id)
