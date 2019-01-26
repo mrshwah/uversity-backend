b57c621c765ffe3502a6db01487489139b7e48e0
sebastianalegre <salegre.morris@gmail.com>
2019-01-26 09:19:43 -0600

    Implemented ReviewList

--- resources/__init__.py
+++ resources/__init__.py
@@ -2,7 +2,8 @@ from flask_restful import Api
 from .auth import Auth
 from .eventbrite import ClientId
 from .users import Users
-from .reviews import Reviews
+from .reviews import Review
+from .reviews import ReviewList
 
 
 def init_app(app):
@@ -10,4 +11,5 @@ def init_app(app):
     api.add_resource(Auth, '/auth')
     api.add_resource(ClientId, '/client_id')
     api.add_resource(Users, '/users')
-    api.add_resource(Reviews, '/reviews')
+    api.add_resource(ReviewList, '/reviews')
+    api.add_resource(Review, '/reviews/<string:id>')
--- resources/reviews.py
+++ resources/reviews.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
-from models.reviews import Review as ReviewModel
 import services.reviews as reviews
+from models.reviews import Review as ReviewModel
 
 post_parser = reqparse.RequestParser()
 args = ['comment',
@@ -15,10 +15,10 @@ for arg in args:
     post_parser.add_argument(arg)
 
 
-class Reviews(Resource):
-    def get(self):
-        reviews = [ob.to_dict() for ob in ReviewModel.objects()]
-        return reviews
+class Review(Resource):
+    def get(self, id):
+        review = Review.objects()
+        return review
 
     def put(self):
         #   This method should be used in the case of updating reviews.
@@ -34,3 +34,22 @@ class Reviews(Resource):
         args = post_parser.parse_args()
         reviews.delete_review(args)
         pass
+
+
+post_parser_list = reqparse.RequestParser()
+args = ['comment',
+        'poster_id',
+        'instructor_id',
+        'class_name',
+        'environment_rating',
+        'organization_rating',
+        'clarity_rating',
+        'expertise_rating']
+for arg in args:
+    post_parser_list.add_argument(arg)
+
+
+class ReviewList(Resource):
+    def get(self):
+        reviews = [ob.to_dict() for ob in ReviewModel.objects()]
+        return reviews
