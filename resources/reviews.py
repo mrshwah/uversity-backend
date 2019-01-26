ec437edb5cf2c1432187b50fc37676414d153110
sebastianalegre <salegre.morris@gmail.com>
2019-01-26 15:38:23 -0600

    minor changes to review

--- resources/reviews.py
+++ resources/reviews.py
@@ -3,7 +3,8 @@ import services.reviews as reviews
 from models.reviews import Review as ReviewModel
 
 post_parser = reqparse.RequestParser()
-args = ['comment',
+
+review_args = ['comment',
         'poster_id',
         'instructor_id',
         'class_name',
@@ -11,18 +12,21 @@ args = ['comment',
         'organization_rating',
         'clarity_rating',
         'expertise_rating']
-for arg in args:
+for arg in review_args:
     post_parser.add_argument(arg)
 
 
 class Review(Resource):
-    def get(self, id):
-        review = ReviewModel.objects(id__exists=id)[0]
+    def get(self, resource_id):
+        review = ReviewModel.objects(id__exists=resource_id)[0]
         review = review.to_dict()
         return {'review': review}
 
-    def put(self):
-        #   This method should be used in the case of updating reviews.
+    def put(self, review_args):
+        args = post_parser.parse_args()
+        review_args = args
+        review = reviews.update_review(review_args)
+        review.save()
         pass
 
     def post(self):
@@ -33,21 +37,21 @@ class Review(Resource):
 
     def delete(self, id):
         reviews = ReviewModel.objects.get(id)
-        reviews.delete_review(args)
+        reviews.delete_review(review_args)
         return True
 
 
-post_parser_list = reqparse.RequestParser()
-args = ['comment',
-        'poster_id',
-        'instructor_id',
-        'class_name',
-        'environment_rating',
-        'organization_rating',
-        'clarity_rating',
-        'expertise_rating']
-for arg in args:
-    post_parser_list.add_argument(arg)
+# post_parser_list = reqparse.RequestParser()
+# args = ['comment',
+#         'poster_id',
+#         'instructor_id',
+#         'class_name',
+#         'environment_rating',
+#         'organization_rating',
+#         'clarity_rating',
+#         'expertise_rating']
+# for arg in args:
+#     post_parser_list.add_argument(arg)
 
 
 class ReviewList(Resource):
