59235a084f02b097499f71dd301937df7cb66bb1
sebastianalegre <salegre.morris@gmail.com>
2019-01-26 11:18:19 -0600

    Added ReferenceField support to users for Reviews

--- services/reviews.py
+++ services/reviews.py
@@ -1,4 +1,5 @@
 from models.reviews import Review
+from models.users import User
 
 
 def calculate_aggregate(cla, env, exp, org):
@@ -7,14 +8,16 @@ def calculate_aggregate(cla, env, exp, org):
 
 #   Create Review
 def create_review(review_args):
+    poster = User.objects(eb_id=review_args['poster_id'])[0]
+
     review = Review(comment=review_args['comment'],
-                    poster_id=review_args['poster_id'],
+                    poster_id=poster,
                     instructor_id=review_args['instructor_id'],
                     class_name=review_args['class_name'],
                     environment_rating=review_args['environment_rating'],
                     organization_rating=review_args['organization_rating'],
                     clarity_rating=review_args['clarity_rating'],
-                    expertise_rating=review_args['expertise_rating'])   
+                    expertise_rating=review_args['expertise_rating'])
     review.aggregate_rating = calculate_aggregate(review.clarity_rating,
                                                   review.environment_rating,
                                                   review.expertise_rating,
