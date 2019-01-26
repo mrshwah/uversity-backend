57230229379dee4c27f5feaccc9c2c0652d03573
Ben Miner <ben.miner@pop.belmont.edu>
2019-01-26 01:25:00 -0600

    fixed uth

--- resources/auth.py
+++ resources/auth.py
@@ -9,7 +9,7 @@ post_parser.add_argument('access_code')
 class Auth(Resource):
     def post(self):
         args = post_parser.parse_args()
-        oauth_token = eb.get_oauth_token(args['access_code'])
+        oauth_token = args['access_code']
         user_args = eb.get_user(oauth_token)
         user = users.create_user(user_args)
         return {'user': user}
