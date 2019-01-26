18690c04122a95faea1b953764add05a257095b7
Ben Miner <ben.miner@pop.belmont.edu>
2019-01-26 13:16:31 -0600

    added add course to user

--- models/users.py
+++ models/users.py
@@ -1,16 +1,22 @@
-from mongoengine import Document, StringField, ImageField, ListField
+from mongoengine import Document, StringField, ImageField, ListField, ReferenceField
+from models.courses import Course
 
 
 class User(Document):
-    eb_id = StringField(unique=True, required= True)
+    eb_id = StringField(unique=True, required=True)
     oauth_token = StringField()
     first_name = StringField(required=True)
     last_name = StringField(required=True)
     email = StringField(required=True)
     # profile_img = ImageField()
     interests = ListField(required=False)
-    course_history = ListField(required=False)
+    course_history = ListField(ReferenceField(Course))
 
     def to_dict(self):
         dictionary = self.to_mongo()
         return {k: v for (k, v) in dictionary.items() if k != '_id'}
+
+    def add_course(self, course_id):
+        course = Course.objects(eb_id=course_id)
+        self.course_history += course
+        self.save()
